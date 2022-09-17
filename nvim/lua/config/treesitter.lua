560d9adeae5dc4bcfdeb8d95b7f753067c9d61a3
ZByongC <zbyongc@gmail.com>
2022-09-17 04:26:19 +0000

    execute 'TSInstall html css scss json', and modify treesitter.lua

--- nvim/lua/config/treesitter.lua
+++ nvim/lua/config/treesitter.lua
@@ -3,7 +3,7 @@ local M = {}
 function M.setup()
 	require("nvim-treesitter.configs").setup {
 		-- A list of parser names, or "all"
-		ensure_installed = {"lua", "javascript", "typescript", "python", "vim", "java"},
+		ensure_installed = {"lua", "javascript", "typescript", "python", "vim", "java", "html", "css", "scss", "json" },
 
 		-- Install languages synchronously (only applied to `ensure_installed`)
 		sync_install = true,
